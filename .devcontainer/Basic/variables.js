2a79b910d8ef28981a3466c17c7737edbba646d2
Manish Bairagi <151638790+Saiyan-pride@users.noreply.github.com>
2025-01-30 11:02:46 +0000

    variables done

---
+++ .devcontainer/Basic/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144533
+let accountEmail = "iammanishbairagi@gmail.com"
+var accountPassword = "113232"
+let accountState;
+console.log(accountEmail)
+console.table([accountId,accountEmail,accountPassword,accountState])
+/*
+prefer not to use var because of issue in block scope and 
+functional scope
+*/
