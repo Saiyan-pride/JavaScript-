752c27bf7824d9a0e6588a98dfbb0aca456e7259
Manish Bairagi <151638790+Saiyan-pride@users.noreply.github.com>
2025-01-30 11:03:50 +0000

    data types done

---
+++ .devcontainer/Basic/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code as newer version
+// alert (3+3) // we are using node , not browser
+
+console.log(3
+    +3
+); // code readability should be high
+
+console.log(typeof(null));
+console.log(typeof(undefined));
+// name => 2 to power 53
+// bigInt
+// string = ""
+// boolean = true/false
+// null => standalone value
+// undefined => empty
+// symbol => unique
+
+// object
+
